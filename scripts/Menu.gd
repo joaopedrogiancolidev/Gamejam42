32724061dac13a7a2c4539d719ddc8e2cb227e18
Letícia Moneda <le.moneda@outlook.com>
2026-06-27 23:04:49 -0300

    feat: esqueleto do fluxo de cenas (menu->lore->tutoriais->hub) + renomeia TemplateLore

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -83,4 +83,4 @@ func _input(event: InputEvent) -> void:
 
 
 func _comecar() -> void:
-	get_tree().change_scene_to_file("res://scenes/Hub.tscn")
+	get_tree().change_scene_to_file("res://scenes/Lore1.tscn")
