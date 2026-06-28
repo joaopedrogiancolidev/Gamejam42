0c03e2a628b67e44e4a13da4d31c3ac5df342d4d
Letícia Moneda <le.moneda@outlook.com>
2026-06-28 04:49:14 -0300

    update scene transitions and tutorial content for improved flow

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -85,4 +85,4 @@ func _input(event: InputEvent) -> void:
 
 
 func _comecar() -> void:
-	get_tree().change_scene_to_file("res://scenes/Lore1.tscn")
+	get_tree().change_scene_to_file("res://scenes/Intro_Cena1.tscn")
